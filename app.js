4ad30f3293759e5146f00b33c3fad835dfeb59b1
sershishkov <ser.shishkov@gmail.com>
2019-02-26 18:31:39 +0200

    05-075

--- app.js
+++ app.js
@@ -1,9 +1,31 @@
-const http = require('http');
+const path = require('path');
 
-const routes = require('./routes');
+const express = require('express');
+const bodyParser = require('body-parser');
 
-console.log(routes.someText);
 
-const server = http.createServer(routes.handler);
+const port = 3000;
+const app = express();
 
-server.listen(3000);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+
+
+app.use((req, res, next) => {
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(port, () => {
+    // console.log(`Server started on port ${port}`);
+})
+
+
+
+
