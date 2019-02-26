f1a28f64d1eebde8b6df59bd7be5f134267af92d
sershishkov <ser.shishkov@gmail.com>
2019-02-26 11:00:59 +0200

    3-029

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const http = require('http');
 const server =  http.createServer((req, res)=>{
 console.log(req.url, req.method, req.headers);
 // process.exit();
+res.setHeader('Content-Type','text/html');
+
+res.write()
 });
 
 server.listen(3000);
