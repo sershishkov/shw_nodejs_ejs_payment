730589324a44fb9c4944f6905d68f2552253a201
sershishkov <ser.shishkov@gmail.com>
2019-02-26 18:54:36 +0200

    start 06

--- routes/shop.js
+++ routes/shop.js
@@ -1,13 +1,12 @@
 const path = require('path');
 
 const express = require('express');
-const router = express.Router();
 
 const rootDir = require('../util/path');
 
+const router = express.Router();
 
 router.get('/', (req, res, next) => {
-
     res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
