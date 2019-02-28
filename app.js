e7609d8c26947b90e0778a71586dc89bc71df03d
sershishkov <ser.shishkov@gmail.com>
2019-02-28 16:00:06 +0200

    6-088

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
+// const expressHbs = require('express-handlebars');
 
 
 
@@ -13,10 +13,16 @@ const app = express();
 // app.set('view engine', 'pug');
 // app.set('views', 'views');
 
-//HANDLEBARS
-app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
-app.set('view engine', 'hbs');
+// //HANDLEBARS
+// app.engine('hbs', expressHbs({ layoutsDir: 'views/layouts/', defaultLayout: 'main-layout', extname: 'hbs' }));
+// app.set('view engine', 'hbs');
+// app.set('views', 'views');
+
+//ejs
+app.set('view engine', 'ejs');
 app.set('views', 'views');
+
+
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
