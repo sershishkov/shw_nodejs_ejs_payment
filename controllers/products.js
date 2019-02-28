0a5a6c22fb5b142189d2d78c75fe886642af981f
sershishkov <ser.shishkov@gmail.com>
2019-02-28 17:52:18 +0200

    07-096

--- controllers/products.js
+++ controllers/products.js
@@ -1,4 +1,4 @@
-const products = [];
+const Product = require('../models/product');
 
 exports.getAddProduct = (req, res, next) => {
 
@@ -12,12 +12,13 @@ exports.getAddProduct = (req, res, next) => {
 
 
 exports.postAddProduct = (req, res, next) => {
-    products.push({ title: req.body.title });
-
+    const newProduct = new Product(req.body.title);
+    newProduct.save();
     res.redirect('/');
 };
 
 exports.getProducts = (req, res, next) => {
+    const products = Product.fetchAll();
     res.render('shop', {
         prods: products,
         pageTitle: 'Shop',
