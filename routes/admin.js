47451626bface07844a2afbd3d4ac4b72e194b9a
sershishkov <ser.shishkov@gmail.com>
2019-03-01 17:43:19 +0200

    9-120

--- routes/admin.js
+++ routes/admin.js
@@ -15,4 +15,6 @@ router.get('/products', adminController.getProducts);
 // /admin/add-product => POST
 router.post('/add-product', adminController.postAddProduct);
 
+router.post('/edit-product/:productId', adminController.getEditProduct);
+
 module.exports = router;
