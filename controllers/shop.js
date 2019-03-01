dde16875a4a30575031e04f020a40bb7b82f0bd0
sershishkov <ser.shishkov@gmail.com>
2019-03-01 15:53:23 +0200

    09-116

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,6 +12,11 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
+  Product.findById(prodId, (product) => {
+
+  })
+
+  res.redirect('/');
 };
 
 exports.getIndex = (req, res, next) => {
